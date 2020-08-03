68e905bdcecea4838e01ce5d78c8969a5e9026f3
Hugo Zheng <zhenghugo515@gmail.com>
2020-08-03 17:24:43 -0400

    New implementation of add item, minor changes

--- src/store/modules/itemStates.js
+++ src/store/modules/itemStates.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 const state = {
   todos: []
@@ -10,29 +11,45 @@ const getters = {
 
 const actions = {
   // fetch todos from api
+  // TODO: replace this url to the real backend
   async fetchTodos({commit}) {
-    // TODO: replace this link to the real backend
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
     commit('setTodos', response.data);
   },
 
+  // TODO: replace this url to the real backend
   async addTodo({commit}, title) {
-    // TODO: replace this link to the real backend
+    // add item to the frontend with a temporary item
+    const tempId = uuidv4();
+    commit('addTodo', {id: tempId, title, completed: false});
+    // update the item with a real id from backend
     const response = await axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false});
-    commit('addTodo', response.data);
+    commit('updateTempId', {tempId, newId: response.data.id})
+
   },
 
-  async deleteTodo({commit}, id){
+  // TODO: replace this url to the real backend
+  async deleteTodo({commit}, id) {
     commit('deleteTodo', id);
-    // TODO: replace this link to the real backend
     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
   }
 };
 
 const mutations = {
-  setTodos: (state, todos) => state.todos = todos,
+  setTodos: (state, todos) => {
+    state.todos = todos;
+  },
   addTodo: (state, newTodo) => state.todos.unshift(newTodo),
-  deleteTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+  deleteTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+  updateTempId: (state, {tempId, newId}) => {
+    state.todos.map(todo => {
+      if (todo.id === tempId) {
+        console.log('before ' + todo.id);
+        todo.id = newId;
+        console.log('after ' + todo.id);
+      }
+    });
+  }
 };
 
 
